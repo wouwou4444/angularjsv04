8f71a1c5a56d9eacb9745fbe6172628fe3ffdff3
wouwou4444 <wouwou4444@outlook.com>
2017-07-02 01:49:16 +0200

    test

--- src/routes.js
+++ src/routes.js
@@ -15,33 +15,7 @@
             templateUrl: 'src/menuapp/templates/home.template.html'
         })
 
-        .state('test', {
-            url: '/test',
-            template: '<div>Test</div>'
-        })
-        
-        // categories
-        .state('menuCategories', {
-            url: '/menu-categories',
-            templateUrl: 'src/menuapp/templates/menu-categories.template.html',
-            controller: 'CategoriesListController as cat',
-            resolve: {
-                categories: ['MenuDataService', function (MenuDataServiceService) {
-                    return MenuDataServiceService.getAllCategories();
-                }]
-                }
-        })
-        // items
-        .state('mainItemsList', {
-            url: '/main-items-list/{catShortName}',
-            templateUrl: 'src/menuapp/templates/main-items-list.template.html',
-            controller: 'ItemsListController as itemsList',
-            resolve: {
-                items: ['$stateParams', 'MenuDataService', function ($stateParams, MenuDataServiceService) {
-                    return MenuDataServiceService.getItemsForCategory($stateParams.catShortName);
-                }]
-                }
-        });
+
 
         
     }
